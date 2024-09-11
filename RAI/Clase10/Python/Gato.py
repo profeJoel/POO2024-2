dd3bdb0507c560f1da0265bea147070e3271f191
profeJoel <joel.torres@ulagos.cl>
2024-09-10 22:11:38 -0300

    Ejemplo Encapsulamiento

--- RAI/Clase10/Python/Gato.py
+++ RAI/Clase10/Python/Gato.py
@@ -4,24 +4,36 @@ class Gato:
     #El constructor en python siempre se llama __init__()
     def __init__(self, name:str, sex:str, age:int, weight:int, color:str, texture:str):
         # En el constructor se definen e instancian los atributos del objeto
-        self.name = name
-        self.sex = sex
-        self.age = age
-        self.weight = weight
-        self.color = color
-        self.texture = texture
+        self.__name = name
+        self.__sex = sex
+        self.__age = age
+        self.__weight = weight
+        self.__color = color
+        self.__texture = texture
     
     def eat(self, comida:str):
-        print(self.name + " va a comer " + comida)
+        print(self.__name + " va a comer " + comida)
 
     def move(self):
-        print(self.name + " se va a mover...")
+        print(self.__name + " se va a mover...")
 
     def meow(self):
-        print(self.name + " va a hacer meowww...")
+        print(self.__name + " va a hacer meowww...")
 
     def purr(self):
-        print(self.name + " se va a ronrronear...")
+        print(self.__name + " se va a ronrronear...")
     
     def hunt_mice(self):
-        print(self.name + " se va a cazar ratones...")
+        print(self.__name + " se va a cazar ratones...")
+
+    def get_name(self):
+        return self.__name
+    
+    def get_color(self):
+        return self.__color
+
+    def set_color(self, nuevo_color:str):
+        if nuevo_color is "blanco":
+            self.__color = nuevo_color
+        else:
+            print("No es permitido el cambio de color")
--- RAI/Clase10/Python/Main.py
+++ RAI/Clase10/Python/Main.py
@@ -7,9 +7,12 @@ if __name__ == "__main__":
     #Para instanciar un objeto debemos utilizar el constructor
     gato1 = Gato("Garfield", "macho", 60, 30, "Naranjo", "esponjoso")
 
-    print("El gato se llama: " + gato1.name)
+    print("El gato se llama: " + gato1.get_name())
     gato1.eat("lassagna")
     gato1.move()
     gato1.meow()
     gato1.purr()
     gato1.hunt_mice()
+    print("El gato tiene color: " + gato1.get_color())
+    gato1.set_color("azul")
+    print("El gato tiene color: " + gato1.get_color())
